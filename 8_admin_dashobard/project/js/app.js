4ebf807c0ae64a3d51ae5d3e5fbe8c0da4efe2cc
heinhtetzan <mmsonestopit@gmail.com>
2020-07-25 02:32:14 +0630

    admin dashobard

--- 8_admin_dashobard/project/js/app.js
+++ 8_admin_dashobard/project/js/app.js
@@ -12,3 +12,26 @@ function go(url) {
         location.href = `${url}`;
     },500);
 }
+
+$(function () {
+    $('[data-toggle="popover"]').popover()
+})
+
+$(".full-screen-btn").click(function () {
+    let current = $(this).closest(".card");
+    current.toggleClass("full-screen-card");
+    if(current.hasClass("full-screen-card")){
+        $(this).html(`<i class="feather-minimize-2"></i>`);
+    }else{
+        $(this).html(`<i class="feather-maximize-2"></i>`);
+    }
+});
+
+let screenHeight = $(window).height();
+let currentMenuHeight = $(".nav-menu .active").offset().top;
+
+if(currentMenuHeight > screenHeight*0.8 ){
+    $(".sidebar").animate({
+        scrollTop:currentMenuHeight-100
+    },1000)
+}
--- 8_admin_dashobard/project/js/dashboard.js
+++ 8_admin_dashobard/project/js/dashboard.js
@@ -7,8 +7,8 @@ let dateArr = ['jul 21','jul 20','jul 19','jul 18','jul 17','jul 16','jul 15','j
 let orderCountArr = [7, 5, 6, 4, 6, 4,8,6,8,9,6];
 let viewerCountArr = [13,12,15,14,20,17,19,16,23,33,16];
 
-var ctx = document.getElementById('ov').getContext('2d');
-var myChart = new Chart(ctx, {
+let ov = document.getElementById('ov').getContext('2d');
+let ovChart = new Chart(ov, {
     type: 'line',
     data: {
         labels: dateArr,
@@ -62,6 +62,61 @@ var myChart = new Chart(ctx, {
             display: true,
             shape:"circle",
             position: 'top',
+            labels: {
+                fontColor: '#333',
+                usePointStyle:true
+            }
+        }
+    }
+});
+
+let orderFromPlace = [5,15,4,9,7];
+let places = ["YGN","MDY","NPY","SHAN","MGW"];
+
+let op = document.getElementById('op').getContext('2d');
+let opChart = new Chart(op, {
+    type: 'doughnut',
+    data: {
+        labels:places,
+        datasets: [{
+            label: '# of Votes',
+            data:orderFromPlace,
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                display:false,
+                ticks: {
+                    beginAtZero: true
+                }
+            }],
+            xAxes: [
+                {
+                    display:false
+                }
+            ]
+        },
+        legend:{
+            display: true,
+            position: 'bottom',
             labels: {
                 fontColor: '#333',
                 usePointStyle:true
